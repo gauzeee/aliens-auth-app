343c07c3feb2898c22ccebacf4efcb6ccf5424fd
gauzeee <gauzeee@gmail.com>
2023-03-17 12:46:41 +0600

    added dist

---
+++ assets/Forgot-164fe909.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-6ab34a51.js";const e=o=>r.jsx(r.Fragment,{children:"Forgot page"});export{e as default};
---
+++ assets/Signup-365d56f0.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-6ab34a51.js";const e=p=>r.jsx(r.Fragment,{children:"Signup page"});export{e as default};
