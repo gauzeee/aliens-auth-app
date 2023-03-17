22d70fa3341403742d2282a72a2b73240cd929a1
gauzeee <gauzeee@gmail.com>
2023-03-17 23:35:11 +0600

    Updates

--- assets/Home-9abeaaac.js
+++
@@ -1,1 +0,0 @@
-import{j as r}from"./index-d351707d.js";const s=t=>r.jsx(r.Fragment,{});export{s as default};
---
+++ assets/Home-b0bcb49e.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-04d5f94e.js";const s=t=>r.jsx(r.Fragment,{});export{s as default};
