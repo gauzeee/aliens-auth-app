c4e05ee857ec91fed9201c34e1ae01e70568ead3
gauzeee <gauzeee@gmail.com>
2023-03-17 13:18:10 +0600

    added base url to router

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,5 +1,10 @@
 import { lazy } from "react";
-import { createBrowserRouter, Link, Navigate, redirect } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Link,
+  Navigate,
+  redirect,
+} from "react-router-dom";
 import { PublicRoute } from "./PublicRoute";
 import { PrivateRoute } from "./PrivateRoute";
 import NotFound from "../pages/NotFound";
@@ -9,46 +14,49 @@ const Login = lazy(() => import("../pages/authorization/Login"));
 const Signup = lazy(() => import("../pages/authorization/Signup"));
 const Forgot = lazy(() => import("../pages/authorization/Forgot"));
 
-export const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <PrivateRoute />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Home />,
-      },
-    ],
-  },
-  {
-    path: "auth",
-    element: <PublicRoute />,
-    errorElement: <NotFound />,
-    children: [
-      {
-        index: true,
-        element: <Navigate to="login" />,
-      },
-      {
-        path: "login",
-        action: async ({ request }) => {
-          const formData = await request.formData();
-          console.log(formData)
-          const updates = Object.fromEntries(formData);
-          console.log(updates)
-          return redirect('/auth/login');
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <PrivateRoute />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true,
+          element: <Home />,
         },
-        element: <Login />,
-      },
-      {
-        path: "signup",
-        element: <Signup />,
-      },
-      {
-        path: "forgot",
-        element: <Forgot />,
-      },
-    ],
-  },
-]);
+      ],
+    },
+    {
+      path: "auth",
+      element: <PublicRoute />,
+      errorElement: <NotFound />,
+      children: [
+        {
+          index: true,
+          element: <Navigate to="login" />,
+        },
+        {
+          path: "login",
+          action: async ({ request }) => {
+            const formData = await request.formData();
+            console.log(formData);
+            const updates = Object.fromEntries(formData);
+            console.log(updates);
+            return redirect("/auth/login");
+          },
+          element: <Login />,
+        },
+        {
+          path: "signup",
+          element: <Signup />,
+        },
+        {
+          path: "forgot",
+          element: <Forgot />,
+        },
+      ],
+    },
+  ],
+  { basename: "/aliens-auth-app/" }
+);
