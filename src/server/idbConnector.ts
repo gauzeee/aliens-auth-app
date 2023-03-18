c3af6b6eb4176855bb8c6401a2a9466958fec692
gauzeee <gauzeee@gmail.com>
2023-03-18 21:42:36 +0600

    added idb versions and increased version

--- src/server/idbConnector.ts
+++ src/server/idbConnector.ts
@@ -1,15 +1,24 @@
+const dbVersion = 3;
 const initDb = () =>
   new Promise(
     (resolve: (db: IDBDatabase) => void, reject: (error: Event) => void) => {
-      const openRequest = indexedDB.open("aliens");
+      const openRequest = indexedDB.open("aliens", dbVersion);
 
-      openRequest.onupgradeneeded = () => {
+      openRequest.onupgradeneeded = (e) => {
         const db = openRequest.result;
+        const upgradeTransaction = (e.target as IDBOpenDBRequest).transaction;
+        let users;
         if (!db.objectStoreNames.contains("users")) {
-          const users = db.createObjectStore("users", {
+          users = db.createObjectStore("users", {
             keyPath: "id",
             autoIncrement: true,
           });
+        } else {
+          if (upgradeTransaction) {
+            users = upgradeTransaction.objectStore("users");
+          }
+        }
+        if (users && !users.indexNames.contains("email_idx")) {
           users.createIndex("email_idx", "email", { unique: true });
         }
         resolve(db);
